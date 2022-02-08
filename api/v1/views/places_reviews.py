8298260c2620bda48664675c8d366a72e6225d1c
Mapu456 <mpaula456@hotmail.com>
2022-02-08 03:26:13 +0000

    FIXING places_reviews.py

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -8,21 +8,22 @@ from models.user import User
 from models import storage
 
 
-@app_views.route('/places/<place_id>/reviews', strict_slashes=False,
-                 methods=['GET'])
-def get_all_reviews(place_id=None):
-    """
-        get reviews relate with a place
-    """
-    place = storage.get(Place, place_id)
-    if not place:
+@app_views.route('/places/<place_id>/reviews',
+                 methods=['GET'], strict_slashes=False)
+def list_reviews_by_place(place_id):
+    """ Retrieves a list of all review objects of a Place. """
+    place = storage.get("Place", place_id)
+    if place is None:
         abort(404)
-    reviews = place.reviews
-    return jsonify([Review.to_dict(review) for review in reviews]), 200
+    reviews_all = []
+    for review in place.reviews:
+        reviews_all.append(review.to_dict())
+
+    return jsonify(reviews_all), 200
 
 
-@app_views.route('/reviews/<review_id>', strict_slashes=False,
-                 methods=['GET'])
+@app_views.route('/reviews/<review_id>',  methods=['GET'],
+                 strict_slashes=False)
 def get_review_id(review_id=None):
     """
         get review based on its id
@@ -33,8 +34,8 @@ def get_review_id(review_id=None):
     return jsonify(Review.to_dict(review)), 200
 
 
-@app_views.route('/reviews/<review_id>', strict_slashes=False,
-                 methods=['DELETE'])
+@app_views.route('/reviews/<review_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_review(review_id=None):
     """
         Deletes a review based on its id and returns an empty JSON
@@ -47,8 +48,8 @@ def delete_review(review_id=None):
     return make_response(jsonify({}), 200)
 
 
-@app_views.route('/places/<place_id>/reviews',
-                 methods=['POST'], strict_slashes=False)
+@app_views.route('/places/<place_id>/reviews', methods=['POST'],
+                 strict_slashes=False)
 def post_review(place_id=None):
     """" Creates a Review object"""
     request_json = request.get_json()
@@ -70,8 +71,7 @@ def post_review(place_id=None):
     return make_response(jsonify(Review.to_dict(review)), 201)
 
 
-@app_views.route('/reviews/<review_id>', strict_slashes=False,
-                 methods=['PUT'])
+@app_views.route('/reviews/<review_id>', methods=['PUT'], strict_slashes=False)
 def update_review(review_id=None):
     """
         Returns the data of a given review
